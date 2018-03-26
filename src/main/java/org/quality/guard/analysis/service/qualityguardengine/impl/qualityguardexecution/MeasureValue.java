0c78d87cb22e162e27a660fe1c3fff48967c6e0e
aboudeffa <aboudeffa@FRP02422.softeam.com>
2018-03-26 15:37:46 +0200

    Add feature STRICT interval agregation
    Fix UI bugs in Cards & Violations history

--- src/main/java/org/quality/guard/analysis/service/qualityguardengine/impl/qualityguardexecution/MeasureValue.java
+++ src/main/java/org/quality/guard/analysis/service/qualityguardengine/impl/qualityguardexecution/MeasureValue.java
@@ -1,16 +1,20 @@
 package org.quality.guard.analysis.service.qualityguardengine.impl.qualityguardexecution;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class MeasureValue implements Comparable<MeasureValue> {
 	
 	private String postDate;
 	private String value;
 	private String measureInstance;
+	private String measureField;
 	
-	public MeasureValue(String postDate, String value, String measureInstance) {
-		super();
+	public MeasureValue(String postDate, String value, String measureInstance,String measurefield) {
 		this.postDate = postDate;
 		this.value = value;
 		this.measureInstance = measureInstance;
+		this.measureField = measurefield;
 	}
 
 	public String getPostDate() {
@@ -24,10 +28,21 @@ public class MeasureValue implements Comparable<MeasureValue> {
 	public String getMeasureInstance() {
 		return measureInstance;
 	}
+	
+
+	public String getMeasurefield() {
+		return measureField;
+	}
 
 	@Override
 	public int compareTo(MeasureValue measureValue) {
-		return Integer.valueOf(this.postDate) - Integer.valueOf(measureValue.postDate);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");   
+		try {
+			return dateFormat.parse(this.getPostDate()).compareTo(dateFormat.parse(measureValue.getPostDate()));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return 0;
 	}
 	
 }
--- target/www/app/manifest.bundle.js
+++ target/www/app/manifest.bundle.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "fb1566bff88615585545"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "69f71bdbaa5081aa3af9"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
