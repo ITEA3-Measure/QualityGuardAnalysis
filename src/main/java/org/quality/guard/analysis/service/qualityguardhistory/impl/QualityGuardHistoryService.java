6751c7fd9801fc59c942349ac4d1bbbf5a1c278d
Antonin Abhervé <antonin.abherve@softeam.fr>
2019-01-21 10:02:37 +0100

    Fix display of issue history

--- src/main/java/org/quality/guard/analysis/service/qualityguardhistory/impl/QualityGuardHistoryService.java
+++ src/main/java/org/quality/guard/analysis/service/qualityguardhistory/impl/QualityGuardHistoryService.java
@@ -33,24 +33,24 @@ public class QualityGuardHistoryService implements IQualityGuardHistoryService {
 		for (int i = 0; i < 10; i++) {
 			if (intervalAgregation.equals("MOY_MIN")) {
 				ZonedDateTime now = ZonedDateTime.now().truncatedTo(ChronoUnit.MINUTES).plusMinutes(1);
-				to = Date.from(now.minusMinutes(i).toInstant());
-				from = Date.from(now.minusMinutes(i+1).toInstant());
+				to = Date.from(now.minusMinutes(i+1).toInstant());
+				from = Date.from(now.minusMinutes(i).toInstant());
 			} if (intervalAgregation.equals("MOY_HH")) {
 				ZonedDateTime now = ZonedDateTime.now().truncatedTo(ChronoUnit.HOURS).plusHours(1);
-				to = Date.from(now.minusHours(i).toInstant());
-				from = Date.from(now.minusHours(i+1).toInstant());
+				to = Date.from(now.minusHours(i+1).toInstant());
+				from = Date.from(now.minusHours(i).toInstant());
 			} if (intervalAgregation.equals("MOY_DD")) {
 				ZonedDateTime now = ZonedDateTime.now().truncatedTo(ChronoUnit.DAYS).plusDays(1);
-				to = Date.from(now.minusDays(i).toInstant());
-				from = Date.from(now.minusDays(i+1).toInstant());
+				to = Date.from(now.minusDays(i+1).toInstant());
+				from = Date.from(now.minusDays(i).toInstant());
 			} if (intervalAgregation.equals("MOY_WW")) {
-				ZonedDateTime now = ZonedDateTime.now(UTC).truncatedTo(ChronoUnit.DAYS).with(DayOfWeek.MONDAY).plusWeeks(1);				
-				to = Date.from(now.minusWeeks(i).toInstant());
-				from = Date.from(now.minusWeeks(i+1).toInstant());
+				ZonedDateTime now = ZonedDateTime.now(UTC).truncatedTo(ChronoUnit.DAYS).with(DayOfWeek.MONDAY).plusWeeks(1);			
+				to = Date.from(now.minusWeeks(i+1).toInstant());
+				from = Date.from(now.minusWeeks(i).toInstant());
 			} if (intervalAgregation.equals("MOY_MM")) {
 				ZonedDateTime now = ZonedDateTime.now(UTC).truncatedTo(DAYS).withDayOfMonth(1).plusMonths(1);
-				to = Date.from(now.minusMonths(i).toInstant());
-				from = Date.from(now.minusMonths(i+1).toInstant());
+				to = Date.from(now.minusMonths(i+1).toInstant());
+				from = Date.from(now.minusMonths(i).toInstant());
 			}
 			
   			GuardStatus status = GuardStatus.SUCCESS;
@@ -80,7 +80,7 @@ public class QualityGuardHistoryService implements IQualityGuardHistoryService {
 		case "MOY_WW":
 			return new SimpleDateFormat("dd-MM").format(dateStatus);
 		case "MOY_MM":
-			return new SimpleDateFormat("MM-yyyy").format(dateStatus);
+			return new SimpleDateFormat("MM-yy").format(dateStatus);
 		}
 		return "";
 	}
